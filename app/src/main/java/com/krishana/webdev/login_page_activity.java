f7e438db5943abfe173554208c0a58ca533c5861
AbhinayPatil <10abhinay10@gmail.com>
2021-09-22 20:15:29 +0530

    abhi test1

--- app/src/main/java/com/krishana/webdev/login_page_activity.java
+++ app/src/main/java/com/krishana/webdev/login_page_activity.java
@@ -2,13 +2,42 @@ package com.krishana.webdev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class login_page_activity extends AppCompatActivity {
+    ImageView backbtn_main;
+    Button login_log;
+    TextView createsignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        backbtn_main = findViewById(R.id.backbtn_main);
+        login_log = findViewById(R.id.login_log);
+        createsignup = findViewById(R.id.createsignup);
+        backbtn_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_page_activity.this,welcomePage.class));
+            }
+        });
+        createsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_page_activity.this,register_page.class));
+            }
+        });
+//        login_log.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(login_page_activity.this,));
+//            }
+//        });
     }
 }
--- app/src/main/java/com/krishana/webdev/welcomePage.java
+++ app/src/main/java/com/krishana/webdev/welcomePage.java
@@ -2,13 +2,41 @@ package com.krishana.webdev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class welcomePage extends AppCompatActivity {
+    Button loginbtn,signinbtn, skip;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+        loginbtn = findViewById(R.id.login_btn);
+        signinbtn= findViewById(R.id.signup_btn);
+        skip = findViewById(R.id.welcome_skip);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(welcomePage.this,login_page_activity.class));
+                finish();
+            }
+        });
+        signinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(welcomePage.this,register_page.class));
+                finish();
+            }
+        });
+
+
+
+
+
     }
 }
