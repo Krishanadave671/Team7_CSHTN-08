d3ea87b85beea136782273b0c320e8f900ab3d30
Raghav_Nair <nairraghav14@gmail.com>
2021-09-22 17:00:32 +0530

    welcome page

---
+++ app/src/main/java/com/krishana/webdev/profilePage.java
@@ -0,0 +1,17 @@
+package com.krishana.webdev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+
+public class profilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
