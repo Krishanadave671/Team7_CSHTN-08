b26af59649674a9e4dba262c6d0fe908c0730065
Krishana Dave <krishnadave671@gmail.com>
2021-09-23 14:08:51 +0530

    register_page created

--- app/src/main/java/com/krishana/webdev/login_page_activity.java
+++ app/src/main/java/com/krishana/webdev/login_page_activity.java
@@ -1,5 +1,6 @@
 package com.krishana.webdev;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +10,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login_page_activity extends AppCompatActivity {
     ImageView backbtn_main;
     Button login_log;
     TextView createsignup;
-    EditText loginEmail,loginPassword;
+    private FirebaseAuth mAuth;
+    TextInputLayout loginEmail,loginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +32,8 @@ public class login_page_activity extends AppCompatActivity {
         backbtn_main = findViewById(R.id.backbtn_main);
         login_log = findViewById(R.id.login_log);
         createsignup = findViewById(R.id.createsignup);
-        loginEmail = findViewById(R.id.loginEmail);
-        loginPassword = findViewById(R.id.loginPasword);
+        loginEmail = findViewById(R.id.Email_log);
+        loginPassword = findViewById(R.id.Password_log);
         backbtn_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,7 +49,21 @@ public class login_page_activity extends AppCompatActivity {
         login_log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(login_page_activity.this,MainActivity.class));
+                String Password = loginPassword.getEditText().getText().toString().trim();
+                String Email = loginEmail.getEditText().getText().toString().trim();
+                mAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(login_page_activity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(login_page_activity.this,MainActivity.class));
+                    }
+                        else{
+                            Toast.makeText(login_page_activity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
     }
